0cd7451f4637037e0df59a352e0ba202fc77867f
Zhu-Lifeng <147527396+Zhu-Lifeng@users.noreply.github.com>
2023-11-16 10:05:51 +0000

    Delete start.py

--- start.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, render_template,redirect,url_for
-
-BPstart = Blueprint('start', __name__)
-
-
-@BPstart.route('/')
-def index():
-    return render_template('start.html')
