5a9e0b460595f74bd0c0c63328ec774623e3401a
Zhu-Lifeng <147527396+Zhu-Lifeng@users.noreply.github.com>
2023-11-16 10:06:20 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+from MainProject import App_Creation,db
+
+app = App_Creation()
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True, host='0.0.0.0', port=5000)
